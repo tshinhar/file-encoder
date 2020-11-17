10d1feafc7d037133f45e9d106be8601db40dd7d
yaeli30994 <74063406+yaeli30994@users.noreply.github.com>
2020-11-17 18:31:29 +0200

    Update utilities.c

--- utilities.c
+++ utilities.c
@@ -1,20 +1,21 @@
-//Authors – Tomer Shinhar 205627524 Yael shwarz 206335010
-//Project – Caesar
+//Authors â€“ Tomer Shinhar 205627524 Yael shwarz 206335010
+//Project â€“ Caesar
 
 //Description - this modul contains general utilities to be used by the other moduels of the project
 
 #include"utilities.h"
 
-char* cut_string_revese(char* string, int string_lenght, char cutting_point) {
-	char* new_string = malloc(string_lenght * sizeof(char));
+char* cut_string_revese(char* string, int string_length, char cutting_point) {
+	int i;
+	char* new_string = (char*)malloc(string_length * sizeof(char));
 	check_malloc(new_string);
-	strcpy_s(new_string, string_lenght, string);
-	int i = string_lenght - 1;
-	while (new_string[i]) {
+	strcpy_s(new_string, string_length, string);
+	for (i = string_length - 2; i>=0; i--)
+	{
 		if (new_string[i] == cutting_point) {
-			new_string[i] = '\0';
+			new_string[i+1] = '\0';
+			break;
 		}
-		i--;
 	}
 	return new_string;
 }
